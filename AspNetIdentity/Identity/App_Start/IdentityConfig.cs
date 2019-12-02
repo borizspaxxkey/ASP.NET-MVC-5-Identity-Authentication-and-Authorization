0db6ce6f046e33c087731ef97a425058b2fbab5b
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-12-02 08:00:32 -0600

    first commit

---
+++ AspNetIdentity/Identity/App_Start/IdentityConfig.cs
@@ -0,0 +1,134 @@
+﻿using Identity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Identity
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            string key = @"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";
+
+            var client = new SendGridClient(key);
+            var from = new EmailAddress("ebukaudeani@gmail.com", "Ebuka Udeani");
+
+            var subject = message.Subject;
+            var to = new EmailAddress(message.Destination, "New User");
+            var plainTextContent = message.Body;
+            var htmlContent = message.Body;
+
+            var email = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            await client.SendEmailAsync(email);
+
+            // Plug in your email service here to send an email.
+            //return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            const string accountSID = @"ACb1a791e5698a69375dda10d28585a0cf";
+            const string authToken = @"9ef257142ac03be95052a492d07cf583";
+
+            TwilioClient.Init(accountSID, authToken);
+
+            var sms = await MessageResource.CreateAsync(
+                body: message.Body,
+                from: new PhoneNumber("(540) 684-5410"),
+                to: new PhoneNumber(message.Destination));
+
+            // Plug in your SMS service here to send a text message.
+            //return Task.FromResult(0);
+        }
+    }
+
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug it in here.
+            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "Your security code is {0}"
+            });
+            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "Security Code",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Configure the application sign-in manager which is used in this application.
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
---
+++ AspNetIdentity/Identity/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Identity.ViewModels;
+using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    //  [Authorize(Roles = "Administrators,Managers")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            var VM = new MyViewModel();
+            VM.IsAdmin = false;
+
+            return View(VM);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            var VM = new MyViewModel();
+            VM.IsAdmin = true;
+
+            return View(VM);
+        }
+    }
+}
